02210e4b902115e63182dc940fb11fd339a9791b
Alex <alex.pappa.1@gmail.com>
2020-05-19 11:01:29 -0400

    updated to allow day filtering

--- auctionPuppeteer.js
+++ auctionPuppeteer.js
@@ -2,8 +2,6 @@ const puppeteer = require("./node_modules/puppeteer");
 const moment = require("./node_modules/moment");
 const _ = require("./node_modules/lodash");
 const baseURL = 'https://www.capitalcityonlineauction.com';
-const homeURL = '/cgi-bin/mncal.cgi?ccoa';
-const fullHomeURL = baseURL + homeURL;
 const startTime = moment();
 const searchTerms = process.argv.slice(2);
 const searchLocations = ["DONN","RIVERSIDE","BLATT","JACKSON","JACKSON RD","BLATT BLVD","GREENPOINTE","GREEN POINTE","43017","43235","43085","43229","43231","43230","43224","43214","43220","43221","43202","43211","43219","43201","43212","43213","43203","43215","43209","43204","43222","43206","43227","43228","43223","43232","43207"]
@@ -18,7 +16,7 @@ function auctionTitleAndLinkScrape() {
                 executablePath: "./node_modules/puppeteer/.local-chromium/win32-674921/chrome-win/chrome.exe"
             });
             const page = await browser.newPage();
-            await page.goto(baseURL);//fullHomeURL
+            await page.goto(baseURL);
             await page.waitFor(5000);
             let urls = await page.evaluate(() => {
                 let results = [];
@@ -43,20 +41,31 @@ function auctionTitleAndLinkScrape() {
 async function dataScrape() {
     let closeAuctions = [];
     let auctionObjects = await auctionTitleAndLinkScrape();
+    let dateFilter = "";
+    switch(moment(searchTerms[0], "dddd").isValid())
+    {
+        case true:
+        dateFilter=searchTerms[0].toUpperCase();
+        break;
+        default:
+            if(searchTerms[0].toUpperCase() === "TONIGHT"){
+                dateFilter=searchTerms[0].toUpperCase();
+            }
+            break;
 
+    }
+    console.log(dateFilter)
     for (let auctionObject = 0; auctionObject <= auctionObjects.length - 1; auctionObject++) {
-        // console.log(`Processing object ${auctionObject + 1} out of ${auctionObjects.length}`)
         let holderObject = {};
         auctionObjects[auctionObject].title = auctionObjects[auctionObject].title.toUpperCase();
         //uppercase it just to make sure everything is consistent
         //auctionObjects[auctionObject].title = auctionObjects[auctionObject].title.slice(auctionObjects[auctionObject].title.indexOf('ENDING')) //nothing before the word ending is important to me
         for(let location of searchLocations){
-            if (auctionObjects[auctionObject].title.includes(location) & (!auctionObjects[auctionObject].title.includes("HAS ENDED"))) {
-                console.log(location);
+            if (auctionObjects[auctionObject].title.includes(location) && (!auctionObjects[auctionObject].title.includes("HAS ENDED") &&(auctionObjects[auctionObject].title.includes(dateFilter)))) {
                 holderObject['url'] = auctionObjects[auctionObject].url;
                 holderObject['title'] = auctionObjects[auctionObject].title;
                 closeAuctions.push(holderObject);
-            } else {}
+            }
         }
     };
     return closeAuctions;
@@ -65,7 +74,6 @@ async function dataScrape() {
 }
 async function crawler() { //maybe before this func i can write a func to read a draft to get the recipient information and then the keywords that they want
     let auctions = await dataScrape();
-     //"Home Decor", "computer Chair", "industrial", "shelf"
 
     const browser = await puppeteer.launch({
         headless: true,
@@ -75,26 +83,31 @@ async function crawler() { //maybe before this func i can write a func to read a
     for (let auction = 0; auction < auctions.length; auction++) {
         await console.log(`working on auction ${auction + 1} of ${auctions.length}`);
         for (let i = 0; i < searchTerms.length; i++) {
-            let page = await browser.newPage();
-            await page.goto(baseURL + auctions[auction].url);
-            await page.waitForSelector("#SearchArea > form:nth-child(1) > input[type=text]:nth-child(2)"); //sometimes it fails here. attempt retry logic?
-            await page.type("#SearchArea > form:nth-child(1) > input[type=text]:nth-child(2)", searchTerms[i]);
-            await page.click("#SearchArea > form:nth-child(1) > input[type=submit]:nth-child(4)");
-            await page.waitFor(1000);
-            let htmlContent = await page.content();
+            try {
+                let page = await browser.newPage();
+                await page.goto(baseURL + auctions[auction].url);
+                await page.waitForSelector("#SearchArea > form:nth-child(1) > input[type=text]:nth-child(2)"); //sometimes it fails here. attempt retry logic?
+                await page.type("#SearchArea > form:nth-child(1) > input[type=text]:nth-child(2)", searchTerms[i]);
+                await page.click("#SearchArea > form:nth-child(1) > input[type=submit]:nth-child(4)");
+                await page.waitFor(1000);
+                let htmlContent = await page.content();
 
 
-            let htmlObj = await {
-                "title": auctions[auction].title,
-                "content": htmlContent,
-                "searchTerm": searchTerms[i],
-                "auctionLink": baseURL + auctions[auction].url
-            };
-            await html.push(htmlObj);
-            await console.log(`pushed ${searchTerms[i]} results to html array`);
-            await page.close();
+                let htmlObj = await {
+                    "title": auctions[auction].title,
+                    "content": htmlContent,
+                    "searchTerm": searchTerms[i],
+                    "auctionLink": baseURL + auctions[auction].url
+                };
+                await html.push(htmlObj);
+                await console.log(`pushed ${searchTerms[i]} results to html array`);
+                await page.close();
+            } catch (e) {
+                console.log(`Could not process the search ${searchTerms[i]}`)
+
+            }
+
         }
-
     }
     await browser.close()
     return html
@@ -106,16 +119,11 @@ async function filterContent() {
 
         if (await !p[i].content.includes("No data found.  Try another search.") || (await !p[i].content.includes("ended"))) {
             let temp = [];
-            try {
                 if (await p[i].content.includes("<tr class=\"DataRow\" id=\"")) {
                     temp = p[i].content.split("<tr class=\"DataRow\" id=\""); //one of these split functions dont work all the time
                 } else {
                     temp = p[i].content.split('<tr class="Datarow');
                 }
-            } catch (e) {
-                console.log(e);
-                console.log("first if else statement")
-            }
             temp.splice(0, 1);
             for (let j = 0; j <= temp.length - 1; j++) {
 
@@ -206,4 +214,3 @@ async function savetoJson() { // file system module to perform file operations
     });
 }
 localBrowsing();
-//savetoJson();
